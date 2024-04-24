a347cffb5da0dda8f268c84370219ce81259044b
Jaroslav Tóth <jaroslav.toth@elisapolystar.com>
2024-04-24 08:57:15 +0200

    Modify IP addressing in VLANs

--- containerlab/openswitch2.sh
+++ containerlab/openswitch2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# set VLAN access ports
+ovs-vsctl set port ovsp1 tag=10
+ovs-vsctl set port ovsp2 tag=20
+ovs-vsctl set port ovsp4 tag=10
+ovs-vsctl set port ovsp5 tag=20
+
 # enable LLDP on each port
 ovs-vsctl set interface R1_OLT lldp:enable=true
 # port from R1_OLT to R1_CPE1
@@ -17,6 +23,13 @@ ovs-vsctl set interface ovsp5 lldp:enable=true
 # port from R2_CMTS to R2_PE1
 ovs-vsctl set interface ovsp6 lldp:enable=true
 
+# add mapping to bridges
+ovs-vsctl add-aa-mapping R1_OLT 0 0
+ovs-vsctl add-aa-mapping R2_CMTS 0 0
+
 # add name visible in LLDP table 
 ovs-vsctl set AutoAttach R1_OLT system_name="R1_OLT"
 ovs-vsctl set AutoAttach R2_CMTS system_name="R2_CMTS"
+
+ovs-vsctl set AutoAttach R1_OLT system_description="Region1_PE2_switch"
+ovs-vsctl set AutoAttach R2_CMTS system_description="Region2_PE1_switch"
