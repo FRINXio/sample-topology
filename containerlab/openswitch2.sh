693874daf02c1c646c67ea878fbd9cb57d4d0147
Jaroslav Tóth <jaroslav.toth@elisapolystar.com>
2024-04-25 13:18:52 +0200

    Configure dot1q interfaces

--- containerlab/openswitch2.sh
+++ containerlab/openswitch2.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# set VLAN access ports
-ovs-vsctl set port ovsp1 tag=10
-ovs-vsctl set port ovsp2 tag=20
-ovs-vsctl set port ovsp4 tag=10
-ovs-vsctl set port ovsp5 tag=20
-
 # enable LLDP on each port
 ovs-vsctl set interface R1_OLT lldp:enable=true
 # port from R1_OLT to R1_CPE1
@@ -33,3 +27,29 @@ ovs-vsctl set AutoAttach R2_CMTS system_name="R2_CMTS"
 
 ovs-vsctl set AutoAttach R1_OLT system_description="Region1_PE2_switch"
 ovs-vsctl set AutoAttach R2_CMTS system_description="Region2_PE1_switch"
+
+# set VLAN access ports
+ovs-vsctl clear port ovsp1 trunks
+ovs-vsctl clear port ovsp2 trunks
+ovs-vsctl clear port ovsp4 trunks
+ovs-vsctl clear port ovsp5 trunks
+
+ovs-vsctl set port ovsp1 vlan_mode=access
+ovs-vsctl set port ovsp2 vlan_mode=access
+ovs-vsctl set port ovsp4 vlan_mode=access
+ovs-vsctl set port ovsp5 vlan_mode=access
+
+ovs-vsctl set port ovsp1 tag=10
+ovs-vsctl set port ovsp2 tag=20
+ovs-vsctl set port ovsp4 tag=10
+ovs-vsctl set port ovsp5 tag=20
+
+# set VLAN trunk ports
+ovs-vsctl clear port ovsp3 trunks
+ovs-vsctl clear port ovsp6 trunks
+
+ovs-vsctl set port ovsp3 trunks=10,20
+ovs-vsctl set port ovsp6 trunks=10,20
+
+ovs-vsctl set port ovsp3 vlan_mode=trunk
+ovs-vsctl set port ovsp6 vlan_mode=trunk
